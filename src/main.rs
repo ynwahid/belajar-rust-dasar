7a304f44380ca0a9df694e16365cc490b4ecfc53
ynwahid <yusufedu99@gmail.com>
2025-08-14 08:02:15 +0700

    slice reference

--- src/main.rs
+++ src/main.rs
@@ -606,3 +606,19 @@ fn test_get_full_name() {
     let full_name = get_full_name(&first_name, &last_name);
     println!("{}", full_name);
 }
+
+#[test]
+fn slice_reference() {
+    // slice uses reference, thus it doesn't transfer the ownership
+    let array: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let slice1: &[i32] = &array[..];
+    println!("{:?}", slice1);
+
+    let slice2: &[i32] = &array[0..5];
+    println!("{:?}", slice2);
+
+    let slice3: &[i32] = &array[5..];
+    let slice4 = slice3;
+    println!("{:?}", slice3);
+}
